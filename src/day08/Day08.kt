ead3ded4222858af5a24525f6ba5d64b18953a2e
Martín Dacosta <martinsilleda@gmail.com>
2024-12-08 11:20:00 +0100

    feat: solve day08 part 1

---
+++ src/day08/Day08.kt
@@ -0,0 +1,58 @@
+package day08
+
+import readInput
+
+fun main() {
+
+    fun isValid(matrix: Array<Array<Char>>, position: Pair<Int, Int>): Boolean {
+        return position.first in matrix.indices && position.second in matrix.indices
+    }
+
+    fun part1(lines: List<String>) {
+        val matrix: Array<Array<Char>> = lines
+            .map { it.toCharArray().toTypedArray() }
+            .toTypedArray()
+
+        val charPositions = mutableMapOf<Char, MutableList<Pair<Int, Int>>>()
+
+        for (i in matrix.indices) {
+            for (j in matrix[i].indices) {
+                val char = matrix[i][j]
+                if (char != '.') charPositions.computeIfAbsent(char) { mutableListOf() }.add(Pair(i, j))
+            }
+        }
+
+        val charsAppearingTwice = charPositions
+            .filterValues { it.size >= 2 }
+        val antinodes = mutableSetOf<Pair<Int, Int>>()
+
+        charsAppearingTwice.forEach { char,list ->
+            for (i in list.indices) {
+                val pair = list[i]
+                for (j in i + 1 until list.size) {
+                    val nextPair = list[j]
+                    //println("$char -> $nextPair - $pair")
+
+                    //Difference
+                    val difference = Pair(nextPair.first - pair.first, nextPair.second - pair.second)
+
+                    val nextPairAntinode = Pair(nextPair.first + difference.first, nextPair.second + difference.second)
+                    val pairAntinode = Pair(pair.first - difference.first, pair.second - difference.second)
+
+                    if (isValid(matrix, nextPairAntinode)) antinodes.add(nextPairAntinode)
+                    if (isValid(matrix, pairAntinode)) antinodes.add(pairAntinode)
+                }
+            }
+        }
+
+        println(antinodes.size)
+    }
+
+    // Or read a large test input from the `src/Day_test.txt` file:
+    val testInput = readInput("day08/Day_test")
+    part1(testInput)
+
+    // Read the input from the `src/Day.txt` file.
+    val input = readInput("day08/Day")
+    part1(input)
+}
