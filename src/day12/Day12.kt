e1ba88f35ad3f4041801f826223ba6d32b0b6413
Martín Dacosta <martinsilleda@gmail.com>
2024-12-12 21:15:10 +0100

    feat: day12 not working

--- src/day12/Day12.kt
+++ src/day12/Day12.kt
@@ -4,13 +4,11 @@ import readInput
 
 fun main() {
 
-    fun calculatePerimeter(positions: Set<Pair<Int, Int>>): List<Pair<Int, Int>> {
+    fun calculateVerticalPerimeter(positions: Set<Pair<Int, Int>>): Int {
         val perimeter = mutableListOf<Pair<Int, Int>>()
 
         // Define all possible directions (up, down, left, right, and diagonals)
         val directions = listOf(
-            Pair(-1, 0), // Up
-            Pair(1, 0),  // Down
             Pair(0, -1), // Left
             Pair(0, 1)  // Right
         )
@@ -26,7 +24,81 @@ fun main() {
             }
         }
 
-        return perimeter
+        val sorted = perimeter.sortedWith(compareBy({ it.second }, { it.first }))
+
+        var count = 0
+
+        for (i in sorted.indices) {
+            if (!perimeter.contains(Pair(sorted[i].first + 1, sorted[i].second))) {
+                count++
+            }
+//            } else if (i + 1 in sorted.indices) {
+//                val next = Pair(sorted[i + 1].first, sorted[i + 1].second)
+//                if (next == sorted[i]) count++
+//            }
+        }
+
+        // Count occurrences and filter repeated pairs
+        val repeatedPairs = sorted.groupingBy { it }
+            .eachCount()
+            .filter { it.value > 1 }
+            .keys
+
+        for (repeatedPair in repeatedPairs) {
+            if (!repeatedPairs.contains(Pair(repeatedPair.first + 1, repeatedPair.second))) {
+                count++
+            }
+        }
+
+        return count
+    }
+
+    fun calculateHorizontalPerimeter(positions: Set<Pair<Int, Int>>): Int {
+        val perimeter = mutableListOf<Pair<Int, Int>>()
+
+        // Define all possible directions (up, down, left, right, and diagonals)
+        val directions = listOf(
+            Pair(-1, 0), // Up
+            Pair(1, 0),  // Down
+        )
+
+        for (position in positions) {
+            for (direction in directions) {
+                val newRow = position.first + direction.first
+                val newCol = position.second + direction.second
+
+                if (!positions.contains(Pair(newRow, newCol))) {
+                    perimeter.add(Pair(newRow, newCol))
+                }
+            }
+        }
+
+        val sorted = perimeter.sortedWith(compareBy({ it.first }, { it.second }))
+
+        var count = 0
+        for (i in sorted.indices) {
+            if (!perimeter.contains(Pair(sorted[i].first, sorted[i].second + 1))) {
+                count++
+            }
+//            } else if (i + 1 in sorted.indices) {
+//                val next = Pair(sorted[i + 1].first, sorted[i + 1].second)
+//                if (next == sorted[i]) count++
+//            }
+        }
+
+        // Count occurrences and filter repeated pairs
+//        val repeatedPairs = sorted.groupingBy { it }
+//            .eachCount()
+//            .filter { it.value > 1 }
+//            .keys
+//
+//        for (repeatedPair in repeatedPairs) {
+//            if (!repeatedPairs.contains(Pair(repeatedPair.first, repeatedPair.second + 1))) {
+//                count++
+//            }
+//        }
+
+        return count
     }
 
     fun exploreNeighbors(
@@ -73,14 +145,14 @@ fun main() {
             if (position in exploredPositions) {
                 //println("Position already explored $position")
             } else {
-                println("Exploring $position with value ${matrix[position.first][position.second]}")
+                //println("Exploring $position with value ${matrix[position.first][position.second]}")
                 exploredPositions.add(position)
                 var keepExploring = true
                 var toExplore = setOf(position)
                 val localExploredPositions = mutableSetOf(position)
 
                 while (keepExploring) {
-                    println("Remainign to explore $toExplore")
+                    //println("Remaining to explore $toExplore")
                     val newAdjacent = mutableListOf<Pair<Int, Int>>()
                     toExplore.forEach { p ->
                         run {
@@ -95,10 +167,12 @@ fun main() {
                     } else {
                         keepExploring = false
                         val localArea = localExploredPositions.count()
-                        val localPerimeter = calculatePerimeter(localExploredPositions).count()
-                        val localPrice = localArea * localPerimeter
+                        //val localPerimeter = calculateVerticalPerimeter(localExploredPositions).count()
+                        val hPerimeter = calculateHorizontalPerimeter(localExploredPositions)
+                        val vPerimeter = calculateVerticalPerimeter(localExploredPositions)
+                        val localPrice = localArea * (hPerimeter + vPerimeter)
 
-                        println("${matrix[position.first][position.second]} -> $localArea * $localPerimeter = $localPrice")
+                        println("${matrix[position.first][position.second]} -> $localArea * ${(hPerimeter + vPerimeter)} = $localPrice")
 
                         price += localPrice
                     }
@@ -116,6 +190,6 @@ fun main() {
 
     // Read the input from the `src/Day.txt` file.
     val input = readInput("day12/Day")
-    part1(input)
     //part1(input)
+    part1(input)
 }
